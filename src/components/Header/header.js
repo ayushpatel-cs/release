bdabfb8970aa0c2a419caeb75138612caf78fd2c
Ayush Patel <ayushpatel@MacBook-Pro-165.local>
2024-09-23 14:02:20 -0400

    homepage update

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,12 +2,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
+import navbarlogo from '../../images/logo.png';
 
 function Header() {
   return (
     <div className="navbar">
       <div className="navbar-container">
-        <h1 className="navbar-title">ReLease</h1>
+        <div className="navbar-left">
+          <img
+            className="navbar-image"
+            src={navbarlogo}  
+            alt="Interior room"
+          />
+          <h1 className="navbar-title">ReLease</h1>
+        </div>
         <div className="navbar-links">
           <Link className="navbar-link" to="/">Home</Link>
           <Link className="navbar-link" to="/login">Login</Link>
