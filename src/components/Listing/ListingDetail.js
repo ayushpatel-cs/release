aea65646622936f06c4a65b990942003c797a968
Akhil Kammila <akhilkammila@gmail.com>
2024-11-20 02:36:37 -0500

    anonymized bids, changed filter def radius, removed prop type

--- src/components/Listing/ListingDetail.js
+++ src/components/Listing/ListingDetail.js
@@ -158,7 +158,7 @@ export default function ListingDetail() {
             {bids?.map((bid) => (
               <tr key={bid.id}>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {bid.bidder?.name || 'Anonymous'}
+                  {'Anonymous'}
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                   ${bid.amount.toLocaleString()}
